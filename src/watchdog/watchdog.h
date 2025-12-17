b82cf3777d2a068ca8ad167c2160f1e982187957
sloggo <switchbyteyt@gmail.com>
2025-12-17 12:29:53 +0000

    fix extern c in display stuff

--- src/watchdog/watchdog.h
+++ src/watchdog/watchdog.h
@@ -4,6 +4,10 @@
 #include "esp_err.h"
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 esp_err_t watchdog_init(uint32_t timeout_seconds);
 
 // Call at start of each monitored task
@@ -12,5 +16,9 @@ esp_err_t watchdog_register_task(void);
 void watchdog_feed(void);
 
 void watchdog_unregister_task(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // WATCHDOG_H
